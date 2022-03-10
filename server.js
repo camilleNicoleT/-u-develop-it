6c2638f20ef0bbb04fea6049757e57d8eae7d0a5
camilleNicole <camilleht@gmail.com>
2022-03-10 18:38:07 -0500

    join method for foreign key

--- server.js
+++ server.js
@@ -70,7 +70,12 @@ db.query(sql, params, (err, result) => {
 
 // GET a single candidate
 app.get('api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
  
 db.query(sql, params, (err, row) => {
@@ -88,7 +93,11 @@ db.query(sql, params, (err, row) => {
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
